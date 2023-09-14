3a716b13a85ad2a956dbe9f08ffd4be2c90dad24
Renee Hsu <piaoyunh@gmail.com>
2023-09-14 14:56:56 +0800

    Setup trending GraphQL query

--- Block Canvas/DiscoverPageViewController.swift
+++ Block Canvas/DiscoverPageViewController.swift
@@ -6,12 +6,20 @@
 //
 
 import UIKit
+import Apollo
 
 class DiscoverPageViewController: UIViewController {
+    let apolloClient = ApolloClient(url: URL(string: "https://api.fxhash.xyz/graphql")!)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        apolloClient.fetch(query: GetTrending.GetTrendingQuery()) { result in
+            guard let data = try? result.get().data else { return }
+            print(data.randomTopGenerativeToken.displayUri)
+            print(data.randomTopGenerativeToken.author.name)
+            print(data.randomTopGenerativeToken.name)
+        }
     }
 
 }
